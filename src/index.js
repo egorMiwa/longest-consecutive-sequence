1149a3342e3eb570ceb88d79d1095a2a4a6085c9
egorMiwa <36693566+egorMiwa@users.noreply.github.com>
2018-03-03 05:10:30 +0300

    z a r a b o t a l o n a 1 0 0

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
 module.exports = function longestConsecutiveLength(array) {
-  // your solution here
+
+function cmp(a,b){
+   return a-b;
+ }
+
+  if(array.length == 0 ) return 0;
+  if(array.length == 1 ) return 1;
+
+  var ans = 0;
+  var count = 1;
+
+  array.sort(cmp);
+
+  for(i=1;i<array.length;i++){
+
+    if(array[i]-array[i-1]==0){
+      continue;
+    }
+
+    if(array[i]-array[i-1]==1){
+      count++;
+    }
+
+    if(array[i]-array[i-1]>1){
+      ans = (count>ans)? count : ans ;
+      count = 1;
+    }
+    
+  }
+
+  return ans;
 }
